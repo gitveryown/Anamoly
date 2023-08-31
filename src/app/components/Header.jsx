1d534a31015950f2c03054ad2eec6f1ad8dc3636
Wright <w.wright@accenture.com>
2023-08-30 23:02:41 -0400

    initializer

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        
+        <ul>
+          <li>Login</li>
+          <li>Sign Up</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
